f517bade9a6fe797299eff7b695e70172e82161a
sanliang <1147693614@qq.com>
2020-09-26 20:09:02 +0800

    Initial commit

---
+++ src/main/java/com/offcn/demo/controller/FirstThymeleafController.java
@@ -0,0 +1,21 @@
+package com.offcn.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstThymeleafController {
+    /**
+     * 访问http://localhost:8080/first
+     * 将数据message填充到templates/index.html
+     * @param model
+     * @return
+     */
+    @GetMapping("/first")
+    public String indexPage(Model model) {
+        String message = "Hello, Thymeleaf!";
+        model.addAttribute("message", message);
+        return "index";
+    }
+}
